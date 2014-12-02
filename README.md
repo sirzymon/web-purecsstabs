4ff83818b93d3d87ea044454c80fd8db021e08ab
Zymon <mo.castaneda@yahoo.com>
2014-12-02 19:28:41 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Pure CSS Tabs
+
+Thanks to phpacademy to the tutorial in youtube
+
+Added some stuff to make it a bit better in my eyes
+
+[Z]
---
+++ js/pageScript.js
@@ -0,0 +1,4 @@
+//javascript
+function showInfo() {
+    alert("A simple activity from phpacademy's tutorial.\n\nphpacademy: https://www.youtube.com/user/phpacademy\n\nBy: Zymon");
+}
